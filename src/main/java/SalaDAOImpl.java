282c3f5e1cb80119d2c0d93bdb4775f857fdb661
Gustavo Izaguirre <izaguirreserna@gmail.com>
2026-03-13 09:52:32 +0100

    primer commit subido

---
+++ src/main/java/SalaDAOImpl.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class SalaDAOImpl implements SalaDAO{
+    @Override
+    public void insertar(Sala sala) {
+
+    }
+
+    @Override
+    public Sala obtener(int id) {
+        return null;
+    }
+
+    @Override
+    public ArrayList<Sala> obtenerTodos() {
+        return null;
+    }
+
+    @Override
+    public void actualizar(Sala sala) {
+
+    }
+
+    @Override
+    public void eliminar(int id) {
+
+    }
+}
