2f5c7566f4d20cb184ecd7d3be2ff29f290d1c29
Gustavo Izaguirre <izaguirreserna@gmail.com>
2026-03-21 18:47:50 +0100

    Cambios realizados

--- src/main/java/DBUtil.java
+++ src/main/java/DBUtil.java
@@ -4,41 +4,13 @@ import java.sql.SQLException;
 
 public class DBUtil {
 
-    // Lo que teniamos -> Cambia Connection a Instancia de la clase
-    private static DBUtil instancia;
 
-    // Lo nuevo -> Atributo tipo connection
-    private Connection conexion;
+    private static final String DB_URL = "jdbc:mysql://localhost:3307/AmongUs";
+    private static final String USER = "root";
+    private static final String PASS = "";
 
-    // Diferencia: La conexión se guarda en el atributo en vez de en el static
-    private DBUtil(){
-        final String DB_URL = "jdbc:mysql://localhost:3307/AmongUs";
-        final String USER = "root";
-        final String PASS = "";
 
-        try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS)) {
-            this.conexion = conn;
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(DB_URL, USER, PASS);
     }
-
-    // Comprobamos atributo static
-    // - Si vacio -> Generamos y devolvemos
-    // - Si lleno -> Solo devolvemos
-    public static DBUtil getInstance(){
-        if (instancia == null) {
-            instancia = new DBUtil();
-        }
-        return instancia;
-    }
-
-    // Getter normal de atributo conexion
-
-    public Connection getConexion() {
-        return this.conexion;
-    }
-
-
 }
-
