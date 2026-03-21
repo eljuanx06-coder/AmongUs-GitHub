e68826d47325487b2176e3c51876b2f07f94f2d6
Gustavo Izaguirre <izaguirreserna@gmail.com>
2026-03-21 19:58:18 +0100

    Realizado

--- src/main/java/SalaDAOImpl.java
+++ src/main/java/SalaDAOImpl.java
@@ -42,6 +42,23 @@ public class SalaDAOImpl implements SalaDAO{
     @Override
     public void actualizar(Sala sala) {
 
+        String sql = "UPDATE sala SET nombre = ?, saboteada = ? WHERE id = ?";
+
+        try (Connection conn = DBUtil.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+
+            pstmt.setString(1, sala.getNombre());
+            pstmt.setBoolean(2, sala.isSaboteada());
+            pstmt.setInt(3, sala.getId());
+
+
+            pstmt.executeUpdate();
+            System.out.println(" Sala actualizada correctamente: " + sala.getNombre());
+
+        } catch (SQLException e) {
+            System.out.println(" Error al actualizar la sala: " + e.getMessage());
+        }
     }
 
     @Override
